9939e5951f5f801e62f57934528366bb1c5b120a
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-01-05 20:41:41 +0530

    [Sangmeshwar] Add: Reading Lines From File Data And Printing

--- FileOperations/FileOperations/Program.cs
+++ FileOperations/FileOperations/Program.cs
@@ -8,17 +8,8 @@ namespace FileOperations
         {
             Console.WriteLine("Welcome to File Operations!");
             string filepath = @"C:\Users\hp\source\repos\FileOperations\FileOperations\Text.txt";
-            string newfilepath = @"C:\Users\hp\source\repos\FileOperations\FileOperations\Text2.txt";
-
             Operations.FileExists(filepath);
             Operations.ReadLines(filepath);
-            Operations.ReadText(filepath);
-            //Operations.FileCopy(filepath, newfilepath);
-            Operations.FileDelete(newfilepath);
-            Operations.FileStreamReader(filepath);
-            Operations.FileStreamWriter(filepath);
-
-
         }
     }
 }
